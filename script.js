e0373c24ce21007dfe05f544d16a302ec054f182
Dazjanay17 <43610721+Dazjanay17@users.noreply.github.com>
2019-03-05 09:27:49 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".button1 ").click(function(){
+ $(".teleport").fadeOut(3000);
+});
+
+
+
+//Spell #2
+
+$(".button2 ").click(function(){
+ $(".fly").slideUp(4000);
+});
+
+
+
+//Spell #3
+
+$(".button3 ").click(function(){
+ $(".Floating").hide(5000);
+});
+
+
+
